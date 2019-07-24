2ad7567dbb9a56e1bfb3d9e8a5c000da6486219e
Brad Mortensen <b.mort86@gmail.com>
2019-07-23 21:41:08 -0600

    adds post request logic

--- src/components/Note.svelte
+++ src/components/Note.svelte
@@ -7,6 +7,17 @@
     );
     console.log(response.status);
   };
+  const handleEdit = async e => {
+    const options = {
+      method: "PUT",
+      body: JSON.stringify(data)
+    }
+    const response = await fetch(
+      `https://lambda-notes-build.herokuapp.com/api/notes/${e.target.id}`,
+      { method: "PUT" }
+    );
+    console.log(response.status);
+  };
 </script>
 
 <style>
@@ -28,7 +39,7 @@
 </style>
 
 <div class="note">
-  <span class="delete" id={note.id} on:click={handleDelete}>X</span>
+  <span class="edit" id={note.id} on:click={handleEdit}>edit</span><span class="delete" id={note.id} on:click={handleDelete}>X</span>
   <h2>{note.title}</h2>
   <p>{note.textBody}</p>
 </div>
