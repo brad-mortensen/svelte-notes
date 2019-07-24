9db4662170fa5a75f133d7f9af22f579e0121a5b
Brad Mortensen <b.mort86@gmail.com>
2019-07-23 21:28:45 -0600

    delete fetch works

--- src/components/Note.svelte
+++ src/components/Note.svelte
@@ -1,7 +1,12 @@
 <script>
   export let note;
-  const handleDelete = () => {
-    
+  const handleDelete = async e => {
+    console.log(e.target.id);
+    const response = await fetch(
+      `https://lambda-notes-build.herokuapp.com/api/notes/${e.target.id}`,
+      { method: "DELETE" }
+    );
+    return response.status;
   };
 </script>
 
@@ -25,5 +30,5 @@
 <div class="note">
   <h2>{note.title}</h2>
   <p>{note.textBody}</p>
-  <span class="delete" on:click={handleDelete}>X</span>
+  <span class="delete" id={note.id} on:click={handleDelete}>X</span>
 </div>
