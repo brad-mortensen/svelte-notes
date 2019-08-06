f0eea141ea3c567f79d2d78cf928d19bde602dc5
Brad Mortensen <b.mort86@gmail.com>
2019-08-06 11:13:56 -0600

    Adding router boilerplate to app.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -56,3 +56,24 @@
     {/if}
   </div>
 </div>
+
+<!-- <script>
+  import { Router, Link, Route } from "svelte-routing";
+  import { Home, About, Blog, BlogPost } from "./routes";
+
+  export let url = "";
+</script>
+
+<Router url="{url}">
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="about">About</Link>
+    <Link to="blog">Blog</Link>
+  </nav>
+  <div>
+    <Route path="blog/:id" component="{BlogPost}" />
+    <Route path="blog" component="{Blog}" />
+    <Route path="about" component="{About}" />
+    <Route path="/"><Home /></Route>
+  </div>
+</Router> -->
